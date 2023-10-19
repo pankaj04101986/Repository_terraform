625da382cf54aa9e3d5262933cdf044faaea2dc1
user.pankaj <user.pankaj.lasurkar@gmail.com>
2023-10-19 19:54:11 +0530

    xyz

---
+++ ec2-instance.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "myec2" {
+  ami           = "ami-0a5ac53f63249fba0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform"
+  }
+
+
+  lifecycle {
+   ignore_changes = all
+  }
+
+
+}
+
+
+
+
+
+
